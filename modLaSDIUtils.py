8a73022772cf930328748298537572890a5820c5
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2022-04-15 15:46:38 -0700

    fixed create_mask_2d to remove the app-specific (-2)

--- modLaSDIUtils.py
+++ modLaSDIUtils.py
@@ -24,36 +24,16 @@ def create_mask_1d(m,b,db):
 def create_mask_2d(nx,ny,m,b,db):
     
   # local
-  Mb=sp.diags([np.ones(nx-2),np.ones(nx-2),np.ones(nx-2)],[0,-1,1],(nx-2,nx-2))
-  M=sp.kron(sp.eye(ny-2),Mb,format="csr")
+  Mb=sp.diags([np.ones(nx),np.ones(nx),np.ones(nx)],[0,-1,1],(nx,nx))
+  M=sp.kron(sp.eye(ny),Mb,format="csr")
 
-  Ib=sp.eye(nx-2)
-  N=sp.kron(sp.diags([np.ones(ny-2),np.ones(ny-2),np.ones(ny-2)],[0,-1,1],(ny-2,ny-2)),Ib,format="csr")
+  Ib=sp.eye(nx)
+  N=sp.kron(sp.diags([np.ones(ny),np.ones(ny),np.ones(ny)],[0,-1,1],(ny,ny)),Ib,format="csr")
 
   local=(M+N).astype('int8')
   I,J,V=sp.find(local)
   local[I,J]=1
   
-#  col_ind=np.array([],dtype='int')
-#  row_ind=np.array([],dtype='int')
-#
-#  for lin_ind in range(m):
-#      j,i=np.unravel_index(lin_ind,(ny-2,nx-2))
-#
-#      E=np.ravel_multi_index((j,np.max((i-1,0))),(ny-2,nx-2))
-#      W=np.ravel_multi_index((j,np.min((i+1,nx-2-1))),(ny-2,nx-2))
-#      S=np.ravel_multi_index((np.max((j-1,0)),i),(ny-2,nx-2))
-#      N=np.ravel_multi_index((np.min((j+1,ny-2-1)),i),(ny-2,nx-2))
-#
-#      col=np.unique([lin_ind,E,W,S,N])
-#      row=lin_ind*np.ones(col.size,dtype='int')
-#
-#      col_ind=np.append(col_ind,col)
-#      row_ind=np.append(row_ind,row)
-#
-#  data=np.ones(row_ind.size,dtype='int')
-#  local2=sp.csr_matrix((data,(row_ind,col_ind)),shape=(m,m))
-
   # basis
   M2 = int(b + db*(m-1))
   basis = np.zeros((m,M2),dtype='int8')
